5f2295fbc4426c26572d09f200256ad832702c1b
Clay <noemail@noemail.com>
2020-03-31 12:47:27 +0800

    linux lib

--- libs_darwin.go
+++ libs_darwin.go
@@ -3,10 +3,9 @@
 package vad
 
 /*
-#cgo CXXFLAGS: -O2 -Wno-delete-non-virtual-dtor -Wunused-function
+#cgo CXXFLAGS: -O3 -Wno-delete-non-virtual-dtor -Wunused-function
 #cgo CXXFLAGS: -Wall -fPIC
 #cgo CXXFLAGS: -I./
-#cgo LDFLAGS: -ldl -lm -lpthread
 #cgo LDFLAGS: -L./mac
 #cgo LDFLAGS: -lfvad
 */
--- libs_linux.go
+++ libs_linux.go
@@ -3,11 +3,10 @@
 package vad
 
 /*
-#cgo CXXFLAGS: -O2 -Wno-delete-non-virtual-dtor -Wunused-function
+#cgo CXXFLAGS: -O3 -Wno-delete-non-virtual-dtor -Wunused-function
 #cgo CXXFLAGS: -Wall -fPIC
 #cgo CXXFLAGS: -I./
-#cgo LDFLAGS: -ldl -luuid -lm -lrt -lpthread -lasound
 #cgo LDFLAGS: -L./linux
-#cgo LDFLAGS: -lvad
+#cgo LDFLAGS: -lfvad
 */
 import "C"
