bcd020c30beeb48ed1bd3e56cfd15fd9b95eb585
cmolocznik <291061+cmolocznik@users.noreply.github.com>
2022-06-12 00:40:21 +0800

    upgraded to use unsafe cgo for hot paths

--- fvad.go
+++ fvad.go
@@ -1,10 +1,49 @@
 package vad
 
 /*
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
 #include "fvad.h"
+
+typedef struct do_fvad_reset_t {
+	size_t ptr;
+} do_fvad_reset_t;
+
+void do_fvad_reset(size_t arg0, size_t arg1) {
+	do_fvad_reset_t* args = (do_fvad_reset_t*)(void*)arg0;
+	fvad_reset((Fvad*)(void*)args->ptr);
+}
+
+typedef struct do_fvad_free_t {
+	size_t ptr;
+} do_fvad_free_t;
+
+void do_fvad_free(size_t arg0, size_t arg1) {
+	do_fvad_free_t* args = (do_fvad_free_t*)(void*)arg0;
+	fvad_free((Fvad*)(void*)args->ptr);
+}
+
+typedef struct do_fvad_process_t {
+	size_t ptr;
+	size_t data;
+	size_t length;
+	int32_t result;
+} do_fvad_process_t;
+
+void do_fvad_process(size_t arg0, size_t arg1) {
+	do_fvad_process_t* args = (do_fvad_process_t*)(void*)arg0;
+	args->result = (int32_t)fvad_process(
+		(Fvad*)(void*)args->ptr,
+		(int16_t*)(void*)args->data,
+		args->length
+	);
+}
+
 */
 import "C"
 import (
+	"github.com/pidato/unsafe/cgo"
 	"reflect"
 	"sync"
 	"unsafe"
@@ -45,16 +84,23 @@ func New() *VAD {
 	}
 }
 
-//
+// Reset
 // Re-initializes a VAD instance, clearing all state and resetting mode and
 // sample rate to defaults.
-//
 func (v *VAD) Reset() {
 	v.mu.Lock()
 	defer v.mu.Unlock()
-	C.fvad_reset(v.ptr)
+	request := struct {
+		ptr *C.Fvad
+	}{
+		ptr: v.ptr,
+	}
+	ptr := uintptr(unsafe.Pointer(&request))
+	cgo.NonBlocking((*byte)(C.do_fvad_reset), ptr, 0)
+	//C.fvad_reset(v.ptr)
 }
 
+// SetMode
 //
 // Changes the VAD operating ("aggressiveness") mode of a VAD instance.
 //
@@ -67,7 +113,6 @@ func (v *VAD) Reset() {
 // ("very aggressive"). The default mode is 0.
 //
 // Returns 0 on success, or -1 if the specified mode is invalid.
-//
 func (v *VAD) SetMode(mode Mode) bool {
 	v.mu.Lock()
 	defer v.mu.Unlock()
@@ -77,6 +122,7 @@ func (v *VAD) SetMode(mode Mode) bool {
 	return C.fvad_set_mode(v.ptr, (C.int)(mode)) == 0
 }
 
+// SetSampleRate
 //
 // Sets the input sample rate in Hz for a VAD instance.
 //
@@ -85,7 +131,6 @@ func (v *VAD) SetMode(mode Mode) bool {
 // sample rates will just be downsampled first.
 //
 // Returns 0 on success, or -1 if the passed value is invalid.
-//
 func (v *VAD) SetSampleRate(sampleRate int32) bool {
 	v.mu.Lock()
 	defer v.mu.Unlock()
@@ -95,6 +140,7 @@ func (v *VAD) SetSampleRate(sampleRate int32) bool {
 	return C.fvad_set_sample_rate(v.ptr, (C.int)(sampleRate)) == 0
 }
 
+// Process
 //
 // Calculates a VAD decision for an audio frame.
 //
@@ -105,21 +151,54 @@ func (v *VAD) SetSampleRate(sampleRate int32) bool {
 // Returns              : 1 - (active voice),
 //                        0 - (non-active Voice),
 //                       -1 - (invalid frame length).
-//
 func (v *VAD) Process(data []int16) Result {
 	v.mu.Lock()
 	defer v.mu.Unlock()
 	if v.closed {
 		return Invalid
 	}
-	return Result(
-		C.fvad_process(
-			v.ptr,
-			(*C.int16_t)(unsafe.Pointer((*reflect.SliceHeader)(unsafe.Pointer(&data)).Data)),
-			(C.size_t)(uint(len(data)))))
+	request := struct {
+		ptr    *C.Fvad
+		data   uintptr
+		length uintptr
+		result int32
+	}{
+		ptr:    v.ptr,
+		data:   (*reflect.SliceHeader)(unsafe.Pointer(&data)).Data,
+		length: uintptr(len(data)),
+		result: 0,
+	}
+	ptr := uintptr(unsafe.Pointer(&request))
+	cgo.NonBlocking((*byte)(C.do_fvad_process), ptr, 0)
+	return Result(request.result)
+	//return Result(
+	//	C.fvad_process(
+	//		v.ptr,
+	//		(*C.int16_t)(unsafe.Pointer((*reflect.SliceHeader)(unsafe.Pointer(&data)).Data)),
+	//		(C.size_t)(uint(len(data)))))
 }
 
-// Closes and frees up native resources.
+func (v *VAD) ProcessNoLock(data []int16) Result {
+	if v.closed {
+		return Invalid
+	}
+	request := struct {
+		ptr    *C.Fvad
+		data   uintptr
+		length uintptr
+		result int32
+	}{
+		ptr:    v.ptr,
+		data:   (*reflect.SliceHeader)(unsafe.Pointer(&data)).Data,
+		length: uintptr(len(data)),
+		result: 0,
+	}
+	ptr := uintptr(unsafe.Pointer(&request))
+	cgo.NonBlocking((*byte)(C.do_fvad_process), ptr, 0)
+	return Result(request.result)
+}
+
+// Close and free up native resources.
 func (v *VAD) Close() error {
 	v.mu.Lock()
 	if v.closed {
@@ -129,7 +208,14 @@ func (v *VAD) Close() error {
 	v.closed = true
 	v.mu.Unlock()
 
-	C.fvad_free(v.ptr)
+	request := struct {
+		ptr *C.Fvad
+	}{
+		ptr: v.ptr,
+	}
+	ptr := uintptr(unsafe.Pointer(&request))
+	cgo.NonBlocking((*byte)(C.do_fvad_free), ptr, 0)
+	//C.fvad_free(v.ptr)
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/go-audio/audio v1.0.0
 	github.com/go-audio/riff v1.0.0
 	github.com/go-audio/wav v1.0.0
+	github.com/pidato/unsafe v0.1.0
 )
